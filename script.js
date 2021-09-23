ce733e0d7a7b1970546a0b60b9d3f01eb1146b8a
Kristina <kristina.m.jefferson@gmail.com>
2021-09-23 08:39:57 -0500

    Add search bar and button and event listeners

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+const sumbitSearch = document.querySelector("#search-button");
+const searchQuery = document.querySelector("#search-bar");
+const buttonClick = sumbitSearch.addEventListener('click', function(){
+    if(searchQuery.value != "") {
+        processQuery(searchQuery.value);
+    }
+});
+
+function processQuery(query){
+    console.log(query);
+}
 
